dd3252e49e41db8f2a281d186467665cd15d52cf
Thiago  Meller Casagrande <thiagomellercasagrande@gmail.com>
2024-10-16 09:52:15 -0300

    autoloopcarousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-import Image from "next/image";
 import { About } from "./components/About";
-import partners from "@/assets/partners.svg";
+import ah from "@/assets/partners/ah.svg";
+import bairroDaJuventude from "@/assets/partners/bairroDaJuventude.svg";
+import fontana from "@/assets/partners/fontana.svg";
+import providencia from "@/assets/partners/providenciaLogo.svg";
+import acate from "@/assets/suporters/acate.png";
+import acif from "@/assets/suporters/acif.png";
+import brde from "@/assets/suporters/brde.png";
+import programaK from "@/assets/suporters/programaK.png";
 import { Plans } from "./components/Plans";
 import { Control } from "./components/Control";
 import { InfoReceivable } from "./components/InfoReceivable";
@@ -11,14 +17,20 @@ import { Testimonials } from "./components/Testimonials";
 import { Contact } from "./components/Contact";
 import { OurPlans } from "./components/OurPlans";
 import { Footer } from "./components/Footer";
+import AutoLoopCarousel from "@/components/Carousels/AutoLoopCarousel";
 // import { PlatformEasy } from "./components/PlatformEasy";
 
 const App = () => {
   return (
     <>
       <About />
-      <section className="hidden w-full mt-14">
-        <Image src={partners} alt="" layout="responsive" />
+      <section className="w-full mt-14">
+        <h4 className="text-caption p-5 pb-10 font-helveticaNeueMedium uppercase text-center">
+          Parceiros
+        </h4>
+        <AutoLoopCarousel
+          carouselItems={[ah, bairroDaJuventude, fontana, providencia]}
+        />
       </section>
       <Plans />
       <Control />
@@ -26,6 +38,12 @@ const App = () => {
       <OperationMap />
       <PlatformEasy />
       <Law />
+      <section className="w-full mt-14">
+        <h4 className="text-caption p-5 pb-10 font-helveticaNeueMedium uppercase text-center">
+          apoiadores prêmios e investidores
+        </h4>
+        <AutoLoopCarousel carouselItems={[acate, acif, brde, programaK]} />
+      </section>
       <Testimonials />
       <Contact />
       <OurPlans />
---
+++ src/components/Carousels/AutoLoopCarousel/index.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import * as React from "react";
+import { useKeenSlider } from "keen-slider/react";
+import Image, { StaticImageData } from "next/image";
+
+interface AutoLoopCarouselProps {
+  carouselItems: string[] | StaticImageData[];
+}
+
+const animation = { duration: 15000, easing: (t: number) => t };
+
+export default function AutoLoopCarousel({
+  carouselItems,
+}: AutoLoopCarouselProps) {
+  const [ref] = useKeenSlider<HTMLDivElement>({
+    loop: true,
+    renderMode: "performance",
+    mode: "free",
+    slides: {
+      perView: 3,
+    },
+    breakpoints: {
+      "(max-width: 1280px)": {
+        slides: {
+          perView: 1,
+        },
+      },
+    },
+    created(s) {
+      s.moveToIdx(5, true, animation);
+    },
+    updated(s) {
+      s.moveToIdx(s.track.details.abs + 5, true, animation);
+    },
+    animationEnded(s) {
+      s.moveToIdx(s.track.details.abs + 5, true, animation);
+    },
+  });
+
+  return (
+    <div ref={ref} className="keen-slider">
+      {carouselItems.map((item, index) => (
+        <div
+          key={index}
+          className="keen-slider__slide place-content-center grid"
+        >
+          <Image src={item} alt="" />
+        </div>
+      ))}
+    </div>
+  );
+}
