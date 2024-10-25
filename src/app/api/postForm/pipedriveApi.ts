5dda6147092d37f9178da1d87c9fb891c804ffcf
Augusto Mandelli <mandelli.augusto@gmail.com>
2024-10-25 17:31:17 -0300

    feat: add testimonials

--- src/app/api/postForm/pipedriveApi.ts
+++ src/app/api/postForm/pipedriveApi.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+export const baseURL = "https://webhook.hablla.com/v1/66f2bcf870c54828e7da82d0";
+
 export const formApi = axios.create({
   baseURL: "https://webhook.hablla.com/v1/66f2bcf870c54828e7da82d0",
 });
