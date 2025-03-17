2f23c602cf96db322156d5ff21fc011ee212be20
Augusto Mandelli <mandelli.augusto@gmail.com>
2025-03-17 13:27:56 +0000

    Edit index.tsx

--- src/components/Buttons/MainButton/index.tsx
+++ src/components/Buttons/MainButton/index.tsx
@@ -8,18 +8,31 @@ export const MainButton = ({
   width,
   responsiveWidth,
   color = "text-white",
-  onClick = () =>
+  ...rest
+}: IButton) => {
+  // Função que lida com o clique no botão
+  const handleOnClick = () => {
+    // Dispara a conversão (verificando se window.gtag existe)
+    if (typeof window !== "undefined" && window.gtag) {
+      window.gtag("event", "conversion", {
+        send_to: "AW-16933724240/mNcwCN6CxqsaENDA0Io_",
+      });
+    } else {
+      console.warn("Gtag ainda não está disponível.");
+    }
+
+    // Em seguida, abre o link do WhatsApp em outra aba
     window.open(
       "https://wa.me/5548991537724?text=Oi%2C%20gostaria%20de%20agendar%20uma%20demonstra%C3%A7%C3%A3o",
       "_blank"
-    ),
-  ...rest
-}: IButton) => {
+    );
+  };
+
   return (
     <button
       className={`${bgColor} ${color} ${responsiveWidth} xl:${width} h-14 px-6 rounded-md hover:shadow-2xl transition-transform hover:scale-105 `}
       {...rest}
-      onClick={onClick}
+      onClick={handleOnClick}
     >
       {children}
     </button>
