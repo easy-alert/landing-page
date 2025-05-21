f64bb2c1ff9384331c0d76d345aa3b2c36440231
Guilherme Roos <guilherme@easyalert.com.br>
2025-05-21 19:03:00 -0300

    refactor: update VideoCard component to use videoId prop instead of src

--- src/app/components/Testimonials/index.tsx
+++ src/app/components/Testimonials/index.tsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 import { VideoCard } from "@/components/Cards/VideoCard";
+import { TestimonialsCarousel } from "@/components/Carousels/TestimonialsCarousel";
+
 import cross10Detail from "../../../assets/details/10CrossDetail.svg";
 import cross8Detail from "../../../assets/details/lawTopRightDetail.svg";
 import cross3Detail from "../../../assets/details/3CrossDetail.svg";
-import Image from "next/image";
-import { TestimonialsCarousel } from "@/components/Carousels/TestimonialsCarousel";
 
 export const Testimonials = () => {
   return (
@@ -12,6 +14,7 @@ export const Testimonials = () => {
         <h4 className="text-caption font-helveticaNeueMedium uppercase text-center">
           Depoimentos
         </h4>
+
         <div className="flex relative w-full justify-center">
           <Image
             src={cross10Detail}
@@ -31,14 +34,14 @@ export const Testimonials = () => {
 
           <div className="flex flex-col gap-16">
             <VideoCard
-              src="https://www.youtube.com/embed/lI6M36t9pJg?si=eWMqsRHOv2Y5tfg8"
+              videoId="lI6M36t9pJg"
               title="Jackson"
               position="Construfase"
               description="A plataforma trouxe uma mudança de cultura, tornando as manutenções preventivas mais eficazes e o condomínio sempre em dia."
             />
 
             <VideoCard
-              src="https://www.youtube.com/embed/72p71wycHac?si=6XwMdDNE4R64vCbj"
+              videoId="72p71wycHac"
               title="Tiago"
               position="Constate"
               description="A implementação da plataforma alerta proprietários para a importância da manutenção preventiva, reduzindo custos e prolongando a vida útil dos empreendimentos."
@@ -46,7 +49,7 @@ export const Testimonials = () => {
             />
 
             <VideoCard
-              src="https://www.youtube.com/embed/OMBar4BqLRU?si=NvI1BEUqytxt7vjt"
+              videoId="OMBar4BqLRU"
               title="Ana Paula"
               position="Mindset"
               description="A plataforma facilitou nosso trabalho, tornando a manutenção transparente para os moradores e liberando nossa equipe para outras atividades essenciais."
