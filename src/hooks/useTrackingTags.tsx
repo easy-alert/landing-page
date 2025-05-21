8720bde60107c54c6b02b76571b42a27b93702a7
Guilherme Roos <guilherme@easyalert.com.br>
2025-05-21 15:14:58 -0300

    feat: add custom hooks for saving and retrieving tracking tags

---
+++ src/hooks/useTrackingTags.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { useSearchParams } from "next/navigation";
+
+const TAG_KEYS = [
+  "utm_campaign",
+  "utm_source",
+  "utm_medium",
+  "utm_term",
+  "gad_source",
+  "gad_campaignid",
+  "gclid",
+  "gbraid",
+  "fbclid",
+];
+
+type StorageType = "sessionStorage" | "localStorage";
+
+export const useSaveTrackingTags = (
+  storageType: StorageType = "sessionStorage"
+) => {
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const storage = window[storageType];
+    const collected: Record<string, string> = {};
+
+    TAG_KEYS.forEach((key) => {
+      const value = searchParams.get(key) || storage.getItem(key);
+
+      if (
+        searchParams.get(key) &&
+        storage.getItem(key) !== searchParams.get(key)
+      ) {
+        storage.setItem(key, searchParams.get(key)!);
+      }
+
+      if (value) {
+        collected[key] = value;
+      }
+    });
+  }, [searchParams, storageType]);
+};
+
+export const useGetTrackingTags = (
+  storageType: StorageType = "sessionStorage"
+) => {
+  const [tags, setTags] = useState<Record<string, string>>({});
+
+  useEffect(() => {
+    const storage = window[storageType];
+    const collected: Record<string, string> = {};
+
+    TAG_KEYS.forEach((key) => {
+      const value = storage.getItem(key);
+
+      if (value) {
+        collected[key] = value;
+      }
+    });
+
+    setTags(collected);
+  }, []);
+
+  return tags;
+};
