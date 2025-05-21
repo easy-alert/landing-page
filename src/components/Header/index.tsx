3b904107b4472e5bf4cc13a020eece6d55a60c32
Guilherme Roos <guilherme@easyalert.com.br>
2025-05-21 18:17:07 -0300

    refactor: reorganize imports and enhance header navigation structure

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,24 @@
 "use client";
 
+import { useState } from "react";
+
 import Image from "next/image";
-import easyAlertLogo from "@/assets/easyAlertLogo.svg";
 import Link from "next/link";
+
+import easyAlertLogo from "@/assets/easyAlertLogo.svg";
+import { icons } from "@/assets/icons";
+
+import { HabllaForm } from "../HabllaForm";
 import { MainButton } from "../Buttons/MainButton";
 import { OutlineButton } from "../Buttons/OutlineButton";
-import { icons } from "@/assets/icons";
-import { useState } from "react";
-import { HabllaForm } from "../HabllaForm";
+
+const headerNavItems = [
+  { name: "Sobre", href: "/#Sobre" },
+  { name: "Funcionalidades", href: "/#Funcionalidades" },
+  { name: "Depoimentos", href: "/#Depoimentos" },
+  { name: "Normas", href: "/#Normas" },
+  { name: "Politica de Privacidade", href: "/politica-de-privacidade-de-dados" },
+];
 
 export const Header = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -18,21 +29,28 @@ export const Header = () => {
 
   return (
     <header className="hidden h-32 bg-headerBackground items-center justify-between gap-20  xl:flex lg:gap-6 lg:px-28">
-      <Image src={easyAlertLogo} alt="logo" />
+      <Link href="/">
+        <Image src={easyAlertLogo} alt="logo" />
+      </Link>
 
       <nav className="text-easyAlertColor">
         <ul className="flex space-x-8">
-          {["Sobre", "Funcionalidades", "Depoimentos", "Normas"].map((item) => (
-            <li key={item} className="transition-transform hover:scale-105">
-              <Link href={`#${item}`}>{item}</Link>
+          {headerNavItems.map((item) => (
+            <li
+              key={item.name}
+              className="transition-transform hover:scale-105"
+            >
+              <Link href={item.href}>{item.name}</Link>
             </li>
           ))}
         </ul>
       </nav>
+
       <div className="flex gap-5">
         <MainButton width="w-64" onClick={() => setIsModalOpen(true)}>
           Agende uma demonstração
         </MainButton>
+
         <OutlineButton
           width="w-32"
           onClick={() =>
@@ -42,6 +60,7 @@ export const Header = () => {
           <Image src={icons.user} alt="" /> Login
         </OutlineButton>
       </div>
+
       {isModalOpen && <HabllaForm onClose={closeModal} />}
     </header>
   );
